a6c78d59900ada1081e5cb2b0128ba3bf9a2782e
Ramazan <ramazankantaev751@email.com>
2025-10-15 09:44:15 +0500

    added and fix homekids

--- src/data/db.js
+++ src/data/db.js
@@ -1,4 +1,6 @@
 export const users = [
     {id: 1, username: "admin", password: "admin123", status: "admin", name: "Рамазан" },
-    {id: 2, username: "kid", password: "kid123", status: "kid", name: "Асем" },
+    {id: 2, username: "Asem", password: "asem1", status: "kid", name: "Асем", clsId: "5A" },
+    {id: 3, username: "Ramazan", password: "ramazan1", status: "kid", name: "Рамазан", clsId: "3A" },
+    {id: 4, username: "Madi", password: "madi1", status: "kid", name: "Мади", clsId: "7B" },
 ]
